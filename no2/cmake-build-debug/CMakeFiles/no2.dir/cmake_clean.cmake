7e4f0abb9c238a7b566e876289db6c43b0b18cd1
AkioM <41733772+AsazimiAme@users.noreply.github.com>
2020-02-03 22:11:13 +0900

    add yent as no2

---
+++ no2/cmake-build-debug/CMakeFiles/no2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/no2.dir/main.cpp.obj"
+  "no2.pdb"
+  "no2.exe"
+  "no2.exe.manifest"
+  "libno2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/no2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ no2/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello\tWorld\n");
+    printf("Windows\tMicrosoft\n");
+    printf("MacOS\tApple\n");
+    return 0;
+}
